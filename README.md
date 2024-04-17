412e2547e27afd23c39909b403b9226e99bd2a01
gerardoMarroquin09 <ma19074@ues.edu.sv>
2024-04-17 16:20:57 -0600

    Pagina Detalle de la oferta, preliminar, y readme.md hecho

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # unichamba-Web
+> Este es un Proyecto de una app de bolsa Universitaria para los jovenes que estudian en la Universidad de El Salvador, Facultad Multidisciplinaria de Occidente.
+App Hecha con React. js, TailWind.css y empaquetado con Vite
+
+###Informacion general
+App Hecha con :
+- React. js (v18.2.0)
+- TailWind.css (v3.4.3)
+- Empaquetado con Vite (v5.2.0)
+
+### Prerrequisitos para poder desplegar:
+- Haber instalado node.js (version mas reciente)
+- Luego de haber instalado node.js instalar yarn de forma global:
+
+```
+$npm install -g yarn
+```
+### Desplegando la aplicacion:
+- Se debe instalar las independencias necesarias  dentro del directorio del proyecto por medio del comando:
+
+```
+$yarn
+```
+- Para echar a andar la app se usa el comando:
+```
+$yarn dev
+```
+
+La app correra en el localhost en el puerto 5173:
+
+[http://localhost:5173](http://localhost:5173)
--- src/pages/DetailsOffer.jsx
+++ src/pages/DetailsOffer.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 const DetailsOffer = () => {
 return(
     <>
-        <h1 className='text-3xl font-medium text-center'>Descripcion de la oferta publicada por el empleador, marroquin</h1>
+    <div className='w-[95%] mx-auto my-12 bg-Gris-claro pb-3 rounded shadow border border-black'>
+        <img className='h-[200px] w-[100%]' src="./public/Fondo-inicio.png" alt="" />
+        <div className='my-4 ml-5 text-black'>
+            <h1 className='font-normal text-4xl mb-0'>Instalacion Electrica</h1>
+            <p className='my-3 ml-2'>Santa Ana- Santa Ana</p>
+            <p className='ml-2 my-2 font-semibold text-gray-600'>Descripcion del Empleo</p>
+            <p className='ml-2 my-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi ea temporibus sit reiciendis deserunt aut inventore nihil sint excepturi quo incidunt itaque, error eaque quod quibusdam non consequuntur optio beatae! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, magnam eaque ad mollitia excepturi illo maxime inventore debitis minus? Reprehenderit eum quos ex eveniet tempore illum assumenda vitae facilis sunt?</p>
+            <p className='ml-2 my-2 font-semibold text-gray-600'> Aptitudes o habilidades</p>
+            <ul className='ml-8 my-2 list-disc'>
+                <li>Tener habilidades manuales</li>
+                <li>Tener capacidad de leer dibujos tecnicos</li>
+            </ul>
+        <button className='ml-4 px-10 py-3 my-5 flex items-center text-white bg-WhatsApp rounded'> <box-icon type='logo' name='whatsapp' color='white'></box-icon>Contacta conmigo</button>
+        </div>
+       </div>
+
     </>
 )
 }
