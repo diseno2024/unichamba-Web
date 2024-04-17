7915124f80ebf8b6b7e92d7d037d2ec5fd5fd599
Elvis <elvisbm12@gmail.com>
2024-04-17 13:22:40 -0600

    Ajustes al Login
    Cambiado un poco la posición de los items.

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -10,8 +10,8 @@ const Login = () => {
     }
 
     return (
-        <div>
-            <header className="flex items-center justify-between p-[15px] border-b-[3px] border-gray-500 bg-Gris-claro">
+        <div className='w-screen h-screen'> 
+            <header className="flex items-start justify-between p-[15px] border-b-[3px] border-gray-500 bg-Gris-claro">
                 <a href="./" className='max-w-[30px]'>
                     <img src="./public/arrow-left-solid.svg" className='w-full' />
                 </a>
@@ -20,14 +20,14 @@ const Login = () => {
                     <img src="./public/LOGO.svg" className='w-full' />
                 </a>
             </header>
-            <div className='flex flex-wrap justify-between items-center mx-[74px] mt-[20px]'>
+            <div className='flex flex-wrap justify-between items-center mx-[74px] mt-[20px] my-auto'>
                 <h1 className='font-roboto font-semibold text-[26px]'>Iniciar sesión para Publicar una oferta</h1>
                 <button className='max-w-[275px] mt-[80px]'>
                     <img src="./public/iniciar-sesion.svg" className='w-full'/>
                 </button>
             </div>
-            <div className='flex flex-wrap items-center justify-between p-[20px]'>
-                <div className='ml-[55px] my-auto mx-auto'>              
+            <div className='flex flex-wrap items-center justify-center p-[20px] pt-[40px]'>
+                <div className='ml-[55px] m-auto'>              
                     <div className='relative'>
                         <label htmlFor='email' className='font-roboto font-normal'>Email</label><br />
                         <img src='./public/bx-user.svg' className='absolute top-[45%] w-[32px] h-[32px] p-[3px] translate-y-[-2%] border-[1px] border-black rounded-md'/>
@@ -59,7 +59,7 @@ const Login = () => {
                     <a href="./" className='font-roboto font-normal h-[40px]'>Olvide mi contraseña</a><br />
                     <button 
                     onClick={onSubmit} 
-                    className='bg-Gris-claro border-[1px] border-black rounded-lg h-[35px] w-[400px]  text-white font-roboto font-semibold mt-[20px]'
+                    className='bg-Azul-oscuro border-[1px] border-black rounded-lg h-[35px] w-[400px]  text-white font-roboto font-semibold mt-[20px]'
                     >
                         Ingresar
                     </button>
