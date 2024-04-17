cb03b60e60cfeb2e0750aafde31dc88449a8b736
eliasronquillo00 <elias.ronquillo00@gmail.com>
2024-04-16 23:45:00 -0600

    Commit inicial

---
+++ src/data/ProfileStudentData.js
@@ -0,0 +1,24 @@
+export const data = [
+    {
+        nombre: 'Fernando Sandoval',
+        ciudad: 'Santa Ana',
+        municipio: 'Santa Ana',
+        contacto: 12345678,
+        email: 'FernandoSandoval123@gmail.com',
+        educacion: 'Universidad de El Salvador',
+        postulaciones: 2,
+        perfilVisitado: 10, //Numero de veces que han visitado el perfil del estudiante
+        descargaCV: 4, //Numero de veces que han descargado el perfil del estudiante
+    },
+    {
+        nombre: 'Lorena Menjivar',
+        ciudad: 'Santa Ana',
+        municipio: 'Ahuachapan',
+        contacto: 87654321,
+        email: 'LoreMenjivar456@gmail.com',
+        educacion: 'Universidad de El Salvador',
+        postulaciones: 6,
+        perfilVisitado: 5, //Numero de veces que han visitado el perfil del estudiante
+        descargaCV: 5, //Numero de veces que han descargado el perfil del estudiante
+    }
+]
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      fontFamily:{
+        "roboto": ['Roboto', 'sans-serif']
+      },
+      colors: {
+        "Azul-Fuerte": "#161A30",
+        "Azul-oscuro": "#31304D",
+        "Gris-claro": "#B6BBC4",
+        "Blanco-cremoso": "#F0ECE5",
+        "Azul-Medianoche": "#2D3250",
+        "Azul-Crepúsculo": "#424769",
+        "Azul-Neblina": "#7077A1",
+        "Naranja Cálido":"#F6B17A",
+        "WhatsApp": "#4FCE5D"
+      }
+    }
+  },
+  plugins: [],
+}
+
