64e7757ae7774128c814b4b715e8430b7d729058
Angel <111787331+angel6464@users.noreply.github.com>
2024-04-18 11:43:48 -0600

    Ajustes Interfaz Crear Oferta

--- src/pages/CreateOffer.jsx
+++ src/pages/CreateOffer.jsx
@@ -32,18 +32,19 @@ console.log(values)
         
         {/*Contenedor principal div con padding para darle espacio alrededor de la tarjeta*/}
         {/*Color pastel*/}
-<div className="bg-Tarjet px-8 pt-4 h-auto w-auto">
-    <div className="flex flex-col lg:flex-row rounded-xl border-2 border-slate-950">
-      {/*Div que contiene la imagen*/}
-      <div className="w-full lg:w-1/3 ">
-          <section>
-           <img
-            src="./imagenOferta.png"
-            alt="./public/ofertaimagen.png"
-            className="w-full h-auto max-w-full lg:max-w-100% object-cover rounded-xl"
-            />
-          </section>
-      </div>
+        <div class="bg-Tarjet px-8 pt-4 h-auto w-auto">
+        <div class="flex flex-col lg:flex-row rounded-xl border border-gray-500 shadow-lg h-auto">
+     
+        <div class="w-full lg:w-1/3 h-auto w-auto">
+            <section class="h-full"> 
+                <img
+                    src="./imagenOferta.png"
+                    alt="./public/ofertaimagen.png"
+                    class="w-full h-full object-cover rounded-xl"
+                />
+            </section>
+        </div>
+  
 
        {/*Div que contiene el formulario*/}
       <div className="w-full lg:w-3/4 ">
@@ -53,80 +54,67 @@ console.log(values)
 
         <form onSubmit={handleSubmit}>
           
-                          {/*fila 1*/}
+                          {/*fila 1 Titulo*/}
                           <div className="flex flex-col lg:flex-row p-2" >
                             <div className="w-full lg:w-4/4 ">
-                              <label className="block text-sm font-medium font-black">Nombre del Cargo o Trabajo *</label>
+                              <label className="block text-sm font-medium font-black">Titulo</label>
                               <input 
-                              id="nombrePuesto"
-                              name="nombrePuesto"
+                              id="Titulo"
+                              name="Titulo"
                               type="text" 
                               className="mx-auto bg-gray-200 rounded-lg border border-black p-2 w-96 mt-1 border-1 border-zinc-950" 
-                       
                               required
                               pattern="[A-Za-z]+"
                               title="Por favor introduce solo letras."/>
                             </div>
                           </div>
 
-                        {/*fila 2 DIRECCION Y EXPERIENCIA*/}
+                        {/*fila 2 Carrera* y imagen*/}
                         <div className="flex flex-col lg:flex-row p-2 ">
                           <div className="w-full lg:w-2/4 ">
-                            <label className="block text-sm font-medium  font-black">Dirección *</label>
-                            <input id="direccion" type="text" className=" bg-gray-200 rounded-lg border border-black p-2 w-96 mt-1 border-1 border-zinc-950" required/>
+                            <label className="block text-sm font-medium  font-black">Carrera*</label>
+                            <input id="Carrera" 
+                            type="text" 
+                            className=" bg-gray-200 rounded-lg border border-black p-2 w-96 mt-1 border-1 border-zinc-950" 
+                            required
+                            />
                         
                           </div>
                           <div className="w-full lg:w-2/4 ">
-                            <label className="block text-sm font-medium  font-black">Carrera*</label>
-                            <input id="carrera" type="text" className=" bg-gray-200 rounded-lg border border-black p-2 w-96 mt-1 border-1 border-zinc-950" required/>
+                        
+                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-black" for="file_input">Escoje una imagen para la oferta</label>
+                            <input class="
+    text-sm border border-gray-300 
+    rounded-lg bg-white text-Azul-Crepúsculo 
+    dark:border-gray-600
+    dark:placeholder-gray-400 p-1.5 w-96 mt-0 
+    hover:bg-Azul-Crepúsculo hover:text-white hover:border-navy
+    " 
+    id="file_input" 
+    type="file"/>
+
 
                           </div>
                         </div>
 
 
+                                      {/*fila 4 Descripcion*/}
+                                      <div className="flex flex-col lg:flex-row p-2" >
+                                      <div className="w-full lg:w-4/4 ">
+                                      <label className="block text-sm font-medium font-black">Descripcion</label>
+                                      </div>
+                                      </div>
 
-                          {/*fila 3 MODALIDAD Y CARGA */}
-                          <div className="flex flex-col lg:flex-row p-2">
-                            <div className="w-full lg:w-2/4 ">
-                              <label className="block text-sm font-medium font-black">Nombre de la Persona Encargada o Empresa</label>
-                              <input id="EmpresaoEncargado" type="text" className=" bg-gray-200 rounded-lg border border-black p-2 w-96 mt-1 border-1 border-zinc-950" required/>
-
-                            </div>
-                            <div className="w-full lg:w-2/4 ">
-                              <label className="block text-sm font-medium  font-black">Requisitos</label>      
-                              <div
-                              id="open-modal-btn"
-                              className="p-2 mt-1 w-96 px-4 bg-gray-300 text-white rounded hover:bg-Azul-Crepúsculo w-40 border-2 border-zinc-950 "
-                           
-                              >
-                              <center><box-icon type='solid' name='file-plus' size='18px' color='Lapislazuli'></box-icon></center>
-                              </div>   
-                            </div>
-                          </div>
-
-
-
-                          {/*fila 4 Descripcion*/}
-                          <div className="flex flex-col lg:flex-row p-2" >
-                            <div className="w-full lg:w-4/4 ">
-                              <label className="block text-sm font-medium font-black">Descripcion</label>
-                            
-                            </div>
-                          </div>
-                        
-
-
-                    {/*fila 5 Descripcion*/}
-
-                    <div className="flex flex-col lg:flex-row p-1 " >
-                  <textarea id="descripcion" rows="8" className="lg:h-auto w-full h-auto border-2 border-zinc-950 bg-gray-200 w-[98%] p-2.5 text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black " placeholder="Descripción del Empleo" required></textarea>
-                  </div>
+                                      {/*fila 5 caja Descripcion*/}
+                                      <div className="flex flex-col lg:flex-row p-1 " >
+                                      <textarea id="descripcion" rows="12" className="lg:h-auto w-full h-auto border-2 border-zinc-950 bg-gray-200 w-[94%] p-2.5 text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black " placeholder="Descripción del Empleo" required></textarea>
+                                      </div>
                   
-                  
+  
 
-                    {/*fila 6*/}
-                    <div className="flex flex-col lg:flex-row  lg:justify-end pr-4" >
-                            <div className="lg:w-3/3 ">
+                    {/*fila 6 boton*/}
+                    <div className="flex flex-col lg:flex-row  lg:justify-end pr-14" >
+                            <div className="lg:w-4/4 ">
                             <button type="submit" className="text-white border-2 w-64 bg-Azul-Crepúsculo border border-black hover:bg-blue-800 hover:text-white font-medium rounded-lg text-sm  py-2.5 ">Crear Oferta</button>
                             </div>
                           </div>
