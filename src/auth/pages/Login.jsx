a9d47c4015a9fe00086bec9d69370c83f71a09b0
GeraCampos21 <anibal212000@gmail.com>
2024-04-19 14:43:09 -0600

    Cambios en Login.jsx

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import NavGeneral from "../../components/NavGeneral";
 
 const Login = () => {
     const [email, setEmail] = useState('')
@@ -9,15 +10,10 @@ const Login = () => {
     }
 
     return (
-        <div className='w-screen h-screen'> 
-            <header className="flex items-start justify-between p-[15px] border-b-[3px] border-gray-500 bg-Gris-claro">
-                <a href="./" className='max-w-[30px] my-auto'>
-                    <img src="./public/arrow-left-solid.svg" className='w-full' />
-                </a>
-
-                <a href="./" className="max-w-[140px] my-auto">
-                    <img src="./public/LOGO.svg" className='w-full' />
-                </a>
+        <div className='w-screen h-screen bg-Blanco-cremoso text-xl'>
+            <header className="bg-Blanco-cremoso">
+                <NavGeneral></NavGeneral>
+               
             </header>
             <div className='flex flex-wrap justify-between items-center mx-[74px] mt-[20px] my-auto'>
                 <h1 className='font-roboto font-semibold text-[26px]'>Iniciar sesión para Publicar una oferta</h1>
@@ -28,26 +24,33 @@ const Login = () => {
             <div className='flex flex-wrap items-center justify-center p-[20px] pt-[40px]'>
                 <div className='ml-[55px] m-auto'>              
                     <div className='relative'>
-                        <label htmlFor='email' className='font-roboto font-normal'>Email</label><br />
-                        <img src='./public/bx-user.svg' className='absolute top-[45%] w-[32px] h-[32px] p-[3px] translate-y-[-2%] border-[1px] border-black rounded-md'/>
-                        <input 
-                        id="emailInput" 
-                        type="email" 
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className='rounded-[5px] border-[1px] bg-white border-black p-[10px_15px] h-[32px] pl-[40px] w-[400px]'
-                        />
+                        <label htmlFor='email' className='font-roboto font-normal ml-7'>User</label><br />
+                        <div className='flex flex-wrap justify-center'>
+                            <box-icon name='user' size='27px'></box-icon>
+                            <input
+                                id="emailInput"
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className='rounded-[5px] border-[1px] bg-Blanco-cremoso border-black p-[10px_15px] h-[32px] pl-[40px] w-[400px]'
+                            />
+
+                        </div>
                     </div>
                     <div className='relative my-[10px]'>
-                        <label htmlFor='password' className='font-roboto font-normal'>Contraseña</label><br />
-                        <img src='./public/bx-lock-alt.svg' className='absolute top-[45%] w-[32px] h-[32px] p-[3px] translate-y-[-2%] border-[1px] border-black rounded-md'/>
-                        <input 
-                        id="passwordInput" 
-                        type="password" 
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className='rounded-[5px] border-[1px] bg-white border-black p-[10px_15px] h-[32px] pl-[40px] w-[400px]'
-                        />
+                        <label htmlFor='password' className='font-roboto font-normal ml-7'>Contraseña</label><br />
+
+                        <div className='flex flex-wrap justify-center'>
+                            <box-icon type='solid' name='lock' size='27px' className=''></box-icon>
+                            <input
+                                id="passwordInput"
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className='rounded-[5px] border-[1px] bg-Blanco-cremoso border-black p-[10px_15px] h-[32px] pl-[40px] w-[400px]'
+                            />
+
+                        </div>
                     </div>
                     <div 
                     id="alerta" 
@@ -55,10 +58,10 @@ const Login = () => {
                     >
                         El email o la contraseña son incorrectos. Por favor, inténtelo de nuevo.
                     </div>
-                    <a href="./" className='font-roboto font-normal h-[40px]'>Olvide mi contraseña</a><br />
+                    <a href="./" className='font-roboto font-normal h-[40px] ml-7'>Olvide mi contraseña</a><br />
                     <button 
                     onClick={onSubmit} 
-                    className='bg-Azul-oscuro border-[1px] border-black rounded-lg h-[35px] w-[400px]  text-white font-roboto font-semibold mt-[20px]'
+                    className='bg-Azul-oscuro border-[1px] border-black rounded-lg h-[35px] w-[400px] ml-7 text-white font-roboto font-semibold mt-[20px]'
                     >
                         Ingresar
                     </button>
