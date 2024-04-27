884e3844cb6bac1a429db3e34ac27e0d41b8f264
SamsPacheco <samuelpacheco234@gmail.com>
2024-04-26 22:41:45 -0600

    vista inicio
    actualizacion de la UI

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -10,73 +10,40 @@ import OfertaLaboral from '../components/OfertaLaboral';
 const Inicio = () => {
   return (
     <>
-      <header className='bg-imagen-fondo bg-cover bg-center h-[600px] flex flex-col justify-between'>
-        <nav className='h-[105px] flex items-center justify-between px-6'>
+      <header className='h-[405px] bg-Dark-Blue space-y-12 rounded-b-3xl relative'>
+        <nav className='h-[105px] flex items-center justify-between px-8'>
           <div>
             <img src="/LOGO.svg" alt="" />
           </div>
+          {/* publicar oferta y auth con google  */}
           <div className='flex gap-5'>
             <NavLink to="/login">
-              <buttons className='text-white font-semibold flex items-center gap-4 border-[2px] border-white h-[55px] px-5 rounded-[8px]'> 
+              <buttons className='text-white font-semibold flex items-center gap-4 bg-Malachite h-[55px] px-5 rounded-[8px]'> 
                 Publicar Oferta 
-                <box-icon name='briefcase' color="white"></box-icon>
+                <span class="material-symbols-outlined">work</span>
                 </buttons>
             </NavLink>
-            <NavLink to="/createAccountStd">
-              <buttons className='bg-white h-[55px] w-[180px] text-Azul-Fuerte font-semibold flex items-center justify-center rounded-[8px]'>Crear Cuenta</buttons>
-            </NavLink>
+            <img src="/iniciar-sesion.svg" alt="" />
           </div>
         </nav>
-        {/* buscador  y button */}
-        <div className='flex flex-col'>
+        <h1 className='text-white font-medium text-4xl w-[35%] relative left-[100px] text-center'>Te ayudamos a encontrar tu primer empleo</h1>
+        
+        {/* buscador */}
+        <div className='flex px-10'>
           {/* inputs */}
-          <div className='border-[1px] border-white h-[50px] rounded-[15px] w-[950px] flex items-center justify-between px-5 mx-auto'>
-                <input type="search" placeholder='Cargo o Puesto' className='bg-inherit placeholder:text-white placeholder:font-normal w-[400px]'/>
-                <box-icon name='search' color="white"></box-icon>
-                <span className='h-[90%] border-[1px] border-white/50 ml-2'></span>
-                <input type="search" placeholder='Municipio' className='bg-inherit placeholder:text-white placeholder:font-normal h-full w-[400px] pl-[15px]'/>
-                <box-icon name='current-location' color="white"></box-icon>
+          <div className='bg-Space-cadet h-[55px] rounded-[10px] w-[950px] flex items-center justify-between px-5 relative'>
+                <input type="search" placeholder='Cargo o Puesto' className='bg-inherit placeholder:text-white placeholder:font-medium w-[400px]'/>
+                <span className='h-[86%] border-[1px] border-white/50 ml-2'></span>
+                <input type="search" placeholder='Municipio' className='bg-inherit placeholder:text-white placeholder:font-medium h-full w-[400px] pl-[15px]'/>
+                <span class="material-symbols-outlined text-white w-[40px] h-[40px] bg-Malachite text-3xl flex items-center justify-center rounded-lg absolute right-2">search</span>
           </div>
-          {/* buttons */}
-          <NavLink to="/studentsPublications" className="mx-auto">
-          <button className='border-[2px] border-white text-white font-semibold w-[280px] mx-auto mt-8 h-[50px] rounded-[5px]'>Buscar Trabajo</button>
-          </NavLink>
         </div>
 
-      <div className='h-[230px] w-full' style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover',backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}></div>
+        <img src="/minerva_sola_white.png" alt="minerva" className='w-[180px] h-[230px] absolute top-24 right-8' />
       </header>
+
       <main className='h-[650px] bg-Blanco-cremoso flex justify-center gap-10 pt-10'>
-        {/* estudiantes */}
-        <section className='flex flex-col items-end'>
-          <div className='w-[700px] h-[450px] bg-Gris-claro py-4 px-4 rounded-[10px] overflow-hidden'>
-              <h1 className='font-normal pb-3'>Estudiantes</h1>
-              <div className='h-[450px] overflow-scroll overflow-x-hidden rounded-[10px] pb-12'>
-              { students.map( (student) => (
-                  <TarjetaPublicacion listStudent={student} key={student.id}/>
-                  ))}
-              </div>
-            </div>
-            <NavLink to='/studentsPublications' className='bg-Azul-oscuro text-white mt-5 mx-10 w-[180px] h-[50px] font-normal rounded-[8px] flex items-center justify-center'>
-            ver más
-            </NavLink>
-        </section>
-
-
-          {/* Anuncios lalborales */}
-          <section className='flex flex-col items-end'>
-            <div className='w-[700px] h-[450px] bg-Gris-claro py-4 px-4 rounded-[10px] overflow-hidden'>
-              <h1 className='font-normal pb-3'>Ofertas Laborales</h1>
-              <div className='h-[450px] overflow-scroll overflow-x-hidden rounded-[10px] pb-12 px-[10px]'>
-              { ofertas.map( (oferta) => (
-                  <OfertaLaboral listStudent={oferta} key={oferta.id}/>
-                  ))}
-              </div>
-            </div>
-            <NavLink to='/OfferExploreStudent' className='bg-Azul-oscuro text-white mt-5 mx-10 w-[180px] h-[50px] font-normal rounded-[8px] flex items-center justify-center'>
-            ver más
-            </NavLink>
-          </section>
-
+        
       </main>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -31,16 +31,13 @@ export default {
         
         "Rich black": "#04061A",
         "Malachite":"#1AD361",
-        "Space cadet":"#2D3250",
+        "Space-cadet":"#2D3250",
         "Blue":"#0A05FE",
         "Dark purple":"#360536",
         "WhatsApp": "#4FCE5D",
         "Navbar": "#D9D9D9",
+        "Dark-Blue": "#04061A"
 
-      },
-      backgroundImage: {
-        "imagen-fondo": "url('/Fondo-inicio.png')",
-        "portada-perfil":"url('Portada-perfil-estudiante.png')",
       },
     }
   },
