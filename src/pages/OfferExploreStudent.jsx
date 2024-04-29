f911411081c5630201d46f3c4663b44febe76575
GeraCampos21 <anibal212000@gmail.com>
2024-04-28 21:03:35 -0600

    OfferExploreStudent
    Cambios en la Interfaz

--- src/pages/OfferExploreStudent.jsx
+++ src/pages/OfferExploreStudent.jsx
@@ -1,55 +1,131 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import ofertas from '../data/Ofertas';
-import OfertaLaboral from '../components/OfertaLaboral'
+import OfertaLaboral from '../components/OfertaLaboral';
 import CarreraFiltro from '../components/CarreraFiltro'
-import AreaTrabajo from '../components/AreaTrabajo'
 import Municipio from '../components/Municipio'
 import { NavLink } from 'react-router-dom'
 
+
 const OfferExploreStudent = () => {
-    return (
-        <>
-            <header>
-                <Navbar />
-            </header>
-            <main className='bg-Blanco-cremoso h-auto mt-[6px] pt-28 space-y-8 '>
 
-                <section className=' w-[95%] h-[100px] bg-Filters border-[1px] border-Azul-oscuro/50 mx-auto rounded-[10px] flex items-center px-5 justify-between'>
+  // Dividir la lista de elementos en dos partes
+  const mitad = Math.ceil(ofertas.length / 2);
+  const primeraColumna = ofertas.slice(0, mitad);
+  const segundaColumna = ofertas.slice(mitad);
 
-                    <div className='w-[700px] flex justify-between'>
-                        <div className='relative'>
-                            <CarreraFiltro />
-                        </div>
-                        <div className='relative'>
-                            <AreaTrabajo />
-                        </div>
-                        <div className='relative'>
-                            <Municipio />
-                        </div>
-                    </div>
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main className='flex h-auto mt-[100px] pt-50 space-y-8 '>
+        <div className=' justify-center ml-14 mt-10 pl-5 py-3 mr-7 min-w-[300px]' >
 
-                    <div className='w-auto flex gap-5 items-center '>
-                        <p className='font-normal'>Ordenar por</p>
-                        <div>
-                            <button className='py-[10px] px-5 rounded-[5px] border-[1px] bg-Azul-Fuerte text-white font-normal'>Relevantes</button>
-                            <button className='py-[10px] px-5 active:border-[1px] border-Azul-Fuerte rounded-[5px] font-normal'>Recientes</button>
-                        </div>
-                    </div>
+          <div className='border-r-2 border-black/40'>
+            <CarreraFiltro />
 
-                </section>
-                <section className='w-[90%] mx-auto '>
-                
-                {ofertas.map( (oferta) => ( 
-                        <NavLink to="/detailsOffer"><OfertaLaboral listStudent={oferta} key={oferta.id}/></NavLink>
+          </div>
+
+          <div className='border-r-2 border-black/40'>
+            <Municipio />
+          </div>
+
+        </div>
+
+
+        <section className=' w-[38%]  '>
+          {/* {ofertas.map( (oferta) => ( 
+                  <OfertaLaboral listStudent={oferta} key={oferta.id}/>
                             
-                ))}
-                </section>
-            </main>
-        </>
-    )
+                ))} */}
+
+          <div className='flex space-x-10'>
+            {/* Primera columna */}
+            <div className='flex-1 '>
+              {primeraColumna.map((elemento, index) => (
+                <OfertaLaboral listStudent={elemento} key={index} />
+              ))}
+            </div>
+
+            {/* Segunda columna */}
+            <div className='flex-1'>
+              {segundaColumna.map((elemento, index) => (
+                <OfertaLaboral listStudent={elemento} key={index} />
+              ))}
+            </div>
+          </div>
+
+
+
+
+        </section>
+
+      </main>
+    </>
+  )
 }
 
 export default OfferExploreStudent
 
  
+/*<section className='w-[90%] mx-auto '>
+                
+                {ofertas.map( (oferta) => ( 
+                        <NavLink to="/detailsOffer"><OfertaLaboral listStudent={oferta} key={oferta.id}/></NavLink>
+                            
+                ))}
+                </section>*/
+
+
+
+                /*
+                 <section className=' w-[95%] h-[100px] bg-Filters border-[1px] border-Azul-oscuro/50 mx-auto rounded-[10px] flex items-center px-5 justify-between'>
+
+                    <div className='w-[700px] flex justify-between'>
+                        <div className='relative'>
+                            <CarreraFiltro />
+                        </div>
+
+                        <div className='relative'>
+                            <Municipio />
+                        </div>
+                    </div>
+
+                   
+
+                </section>
+*/
+
+
+
+
+
+
+
+// const ComponentePaginacion = ({ elementosFiltrados }) => {
+//   // Dividir la lista de elementos en dos partes
+//   const mitad = Math.ceil(elementosFiltrados.length / 2);
+//   const primeraColumna = elementosFiltrados.slice(0, mitad);
+//   const segundaColumna = elementosFiltrados.slice(mitad);
+
+//   return (
+//     <div style={{ display: 'flex' }}>
+//       {/* Primera columna */}
+//       <div style={{ flex: 1 }}>
+//         {primeraColumna.map((elemento, index) => (
+//           <div key={index}>{/* Renderizar el componente de la card aquí */}</div>
+//         ))}
+//       </div>
+
+//       {/* Segunda columna */}
+//       <div style={{ flex: 1 }}>
+//         {segundaColumna.map((elemento, index) => (
+//           <div key={index}>{/* Renderizar el componente de la card aquí */}</div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+
