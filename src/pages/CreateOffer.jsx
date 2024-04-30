7165e4162a31a368e0728a67e51588bf7d3f4030
Angel <111787331+angel6464@users.noreply.github.com>
2024-04-30 16:45:20 -0600

    Update Crear Oferta

--- src/pages/CreateOffer.jsx
+++ src/pages/CreateOffer.jsx
@@ -1,29 +1,36 @@
 import React, { useState } from "react";
 import NavGeneral from '../components/NavGeneral'
-
-
-
+import listCarrersOrden from "../data/orderCarrers";
+import municipio from "../data/municipio";
 
 const CreateOffer = () => {
   const initialStateValues = {
     nombrePuesto: "",
   }
-
-  const [values, setValues] = useState(initialStateValues)
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value })
-
-  }
-
+ 
   const handleSubmit = (e) => {
     e.preventDefault(); // Envío del formulario
     window.location.href = '/inicio'; // Redirigir a la página de inicio
   }
 
-  const onSubmit = () => {
-    window.location.href = '/inicio'
-  }
+//Definimos las variables de useState para que nos funcionen en los dos metodos para obtener la data de carreras y ubicacion
+  const [isOpen, setIsOpen] = useState(false);
+
+//para obtener datos de las carreras---------------------
+  const [selectedCarrera, setSelectedCarrera] = useState('');
+  const handleSelectChangeCarrera = (event) => {
+    setSelectedCarrera(event.target.value);
+    setIsOpen(false);
+  };
+//-----------------------------------------------------------------
+
+//para obtener Ubicacion--------------------------------------
+const [selectedUbicacion, setSelectedUbicacion] = useState('');
+const handleSelectChangeUbcacion= (event) => {
+  setSelectedUbicacion(event.target.value);
+  setIsOpen(false);
+};
+//-----------------------------------------------------------------
 
   return (
     <>
@@ -54,46 +61,69 @@ const CreateOffer = () => {
                 <form onSubmit={handleSubmit} className="h-[615px] space-y-2">
 
                   <p className="pt-5 pl-6 font-roboto font-bold text-lg">Crea una oferta</p>
-                  {/*fila 1 Titulo*/}
+                  {/*fila 1 Titulo y ubicacion*/}
                   <div className="flex flex-col lg:flex-row p-2" >
-                    <div className="w-full lg:w-4/4 ">
-                      <label className="block text-sm font-medium">Titulo</label>
+                    <div className="w-full lg:w-2/4 ">
+                      <label className="block text-sm font-medium">Titulo*</label>
                       <input
                         id="Titulo"
                         name="Titulo"
                         type="text"
-                        className="mx-aut bg-Blanco-cremoso font-light rounded-lg border border-black p-2 w-96 mt-1 border-1 border-zinc-950"
+                        className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ... mx-aut bg-Blanco-cremoso font-light rounded-lg border border-black p-2 w-96 mt-1 border-1 border-Dark-purple"
                         required
                         pattern="[A-Za-z]+"
                         title="Por favor introduce solo letras." />
+                    </div>
+                    <div className="w-full lg:w-2/4 ">
+                    <label className="block text-sm font-medium ">Ubicacion(municipio)</label>
+                      <select
+                      className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ... mx-aut bg-Blanco-cremoso font-light rounded-lg border border-black p-2 w-96 mt-1 border-1 border-Dark-purple"
+                      onClick={() => setIsOpen((prev) => !prev)}
+                      onChange={handleSelectChangeUbcacion}
+                      value={selectedUbicacion}
+                    >  
+                      <option value="">Seleccione un Municipio</option>
+                        {municipio.map(({ municipio, id }) => (
+                          <option value={municipio} key={id}>{municipio}</option>
+                        ))}
+                      </select>
+
                     </div>
                   </div>
 
                   {/*fila 2 Carrera* y imagen*/}
                   <div className="flex flex-col lg:flex-row p-2 ">
                     <div className="w-full lg:w-2/4 ">
-                      <label className="block text-sm font-medium ">Carrera*</label>
-                      <input id="Carrera"
-                        type="text"
-                        className=" bg-Blanco-cremoso font-light rounded-lg border border-black p-2 w-96 mt-1 border-1 border-zinc-950"
-                        required
-                      />
 
+                    <label className="block text-sm font-medium">Carrera(opcional)</label>
+                    <select
+                      className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ... mx-aut bg-Blanco-cremoso font-light rounded-lg border border-black p-2 w-96 mt-1 border-1 border-Dark-purple"
+                      onClick={() => setIsOpen((prev) => !prev)}
+                      onChange={handleSelectChangeCarrera}
+                      value={selectedCarrera}
+                    >  
+                   <option value="">Seleccione una carrera</option>
+                      {listCarrersOrden.map(({ nombre, id }) => (
+                        <option value={nombre} key={id}>{nombre}</option>
+                      ))}
+                    </select>
                     </div>
+
                     <div className="w-full lg:w-2/4">
 
                       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-                        for="file_input">Escoje una imagen para la oferta</label>
+                        for="file_input">Selecciona Imagen(opcional)</label>
                       <input className="
-                              text-sm border border-gray-300 
+                      transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ...
+                              text-sm border border-Dark-purple 
                               rounded-lg text-Azul-Crepúsculo font-light 
-                              dark:border-gray-600
+                              border-Dark-purple
                               dark:placeholder-gray-400 p-1.5 w-96 mt-0 
                               px-3 
                               "
                         id="file_input"
                         type="file"
-                        required />
+                        />
 
 
                     </div>
@@ -109,7 +139,7 @@ const CreateOffer = () => {
 
                   {/*fila 5 caja Descripcion*/}
                   <div className="flex flex-col lg:flex-row p-1 " >
-                    <textarea id="descripcion" rows="12" className="lg:h-auto w-full h-auto border-2 border-zinc-950 bg-Blanco-cremoso p-2.5 text-sm text-gray-900 rounded-lg font-normal "
+                    <textarea id="descripcion" rows="12" className="lg:h-auto w-full h-auto border-2 border-Dark-purple bg-Blanco-cremoso p-2.5 text-sm text-gray-900 rounded-lg font-normal "
                       placeholder="Escriba una breve descripcion sobre el empleo"
                       required></textarea>
                   </div>
