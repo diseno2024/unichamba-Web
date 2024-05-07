4dc2c62780150a93bb34050dd6f4c9ed1a6e213b
Elvis <elvisbm12@gmail.com>
2024-05-07 10:51:45 -0600

    Actualizado AdministradoresUserAdmin
    Agregada la funcionalidad de agregar y eliminar administradores.

--- src/components/AdministradoresUserAdmin.jsx
+++ src/components/AdministradoresUserAdmin.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 import { db } from "../data/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDocs } from "firebase/firestore";
 
 const AdministradoresUserAdmin = () => {
   const [administradores, setAdministradores] = useState([])
@@ -20,13 +20,22 @@ const AdministradoresUserAdmin = () => {
     return fetchAdministradores
   }, [])
 
+  const agregarAdmin = async (name, email) => {
+    await addDoc(collection(db, 'administradores'), { correo: email, nombre: name })
+    fetchAdministradores()
+  }
+
+  const eliminarAdmin = async (id) => {
+    await deleteDoc(doc(db, 'administradores', id));
+    fetchAdministradores();
+  }
+
   // Modal para confirmar la eliminación
   const modalEliminarAdmin = (admin) => {
     Swal.fire({
       title: `Eliminar a ${admin.nombre}`,
       html: `
       <div>
-        <img src="/foto-perfil.jpg" alt="Foto de perfil" class="w-[12%] mx-auto mb-4 rounded-full">
         <p>Correo electrónico: ${admin.correo}</p>
       </div>
       <hr class="my-4">
@@ -39,7 +48,8 @@ const AdministradoresUserAdmin = () => {
       confirmButtonText: "Sí, eliminar",
     }).then((result) => {
       if (result.isConfirmed) {
-        // Aquí se puede implementar el metodo para eliminar al administrador
+        // Aquí se puede implementar el metodo para eliminar al administrado
+        eliminarAdmin(admin.id)
         Swal.fire("Eliminado", `${admin.nombre} ha sido eliminado`, "success");
       }
     });
@@ -49,27 +59,27 @@ const AdministradoresUserAdmin = () => {
   // Modal para agregar administradores
   const modalAgregarAdministradores = () => {
     Swal.fire({
-      title: "Agregar Correo Electrónico",
-      html: '<input id="correo" name="correo" class="swal2-input" placeholder="Ingrese su correo electrónico">',
+      title: "Agregar Correo Electrónico y Nombre",
+      html: `<input id="correo" name="correo" class="swal2-input" placeholder="Ingrese su correo electrónico">
+      <input id="nombre" name="nombre" class="swal2-input" placeholder="Ingrese su nombre">`
+      ,
       showCancelButton: true,
       confirmButtonText: "Agregar",
       cancelButtonText: "Cancelar",
       showLoaderOnConfirm: true,
       preConfirm: () => {
         const email = document.getElementById("correo").value;
-        if (!email) {
-          Swal.showValidationMessage("Por favor, ingrese un correo electrónico válido");
+        const nombre = document.getElementById("nombre").value;
+        if (!email || !nombre) {
+          Swal.showValidationMessage("Por favor, complete ambos campos");
         } else {
-          modalConfirmacion(); // Llamada a modalConfirmacion
-          /*aqui puede ir el metodo de firebase para guardar el correo que sera admin a traves del
-                input con la variable correo,podria ser que antes de enviar el correo
-                haga una busqueda en firebase para ver si existe el registro y 
-                si existe lo guardara como admin*/
+          agregarAdmin(nombre, email);
         }
       },
       allowOutsideClick: () => !Swal.isLoading(),
     });
   };
+  
 
   // Modal que confirma que se guardó el correo como admin
   const modalConfirmacion = () => {
@@ -85,6 +95,7 @@ const AdministradoresUserAdmin = () => {
       customClass: {
         popup: "text-black",
       },
+
     });
   };
 
@@ -102,16 +113,12 @@ const AdministradoresUserAdmin = () => {
         {administradores.map((admin) => (
           <div key={admin.id} className="transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100 ... w-[80%] h-[150px] border-b-2 border-gray-500 grid grid-cols-5 hover:border-b-2 hover:border-blue-500">
             <div className="col-span-4 flex items-center">
-              <img src="/foto-perfil.jpg" alt="" className="w-[12%] rounded-full border-8" />
               <div className="ml-4">
                 <h1 className="font-normal text-Blue text-xl">{admin.nombre}</h1>
                 <p className="font-light">{admin.correo}</p>
               </div>
             </div>
             <div className="flex justify-center gap-6 items-center">
-              <span className="cursor-pointer ... material-symbols-outlined text-3xl text-Blue" key={admin.id}>
-                edit
-              </span>
               <span className="cursor-pointer ... material-symbols-outlined text-3xl text-red-600" key={admin.id} onClick={() => modalEliminarAdmin(admin)}>
                 delete
               </span>
