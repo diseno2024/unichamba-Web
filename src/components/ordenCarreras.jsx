9400ea453fe35f6392b3b593272c1a7357112c4f
Rabindranath <cr20009@ues.edu.sv>
2024-06-05 13:17:49 -0600

    extraccion de carreras de firebase y ordenamiento alfabeticamente

--- src/components/ordenCarreras.jsx
+++ src/components/ordenCarreras.jsx
@@ -1,11 +1,36 @@
-import React, { useState } from "react";
-import listCarrersOrden from "../data/orderCarrers";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { db } from "../data/firebase"; // Ajusta la ruta según la ubicación de tu archivo de configuración de Firebase
+import { collection, getDocs } from "firebase/firestore";
 
-const OrdenarCarreras = ({onSelect}) => {
+const OrdenarCarreras = ({ onSelect }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedCarrera, setSelectedCarrera] = useState("");
+  const [carreras, setCarreras] = useState([]); // Estado para almacenar las carreras
   const location = useLocation();
+
+  useEffect(() => {
+    // Función para obtener las carreras de Firestore
+    const fetchCarreras = async () => {
+      try {
+        const carrerasCollection = collection(db, "carreras");
+        const carrerasSnapshot = await getDocs(carrerasCollection);
+        const carrerasList = carrerasSnapshot.docs.map(doc => ({
+          id: doc.id,
+          carrera: doc.data().carrera
+        }));
+        
+        // Ordenar las carreras alfabéticamente por su nombre
+        carrerasList.sort((a, b) => a.carrera.localeCompare(b.carrera));
+        
+        setCarreras(carrerasList);
+      } catch (error) {
+        console.error("Error obteniendo carreras: ", error);
+      }
+    };
+
+    fetchCarreras(); // Llamar a la función para obtener las carreras cuando el componente se monta
+  }, []);
 
   const handleSelectChange = (event) => {
     const carreraSeleccionada = event.target.value;
@@ -13,8 +38,6 @@ const OrdenarCarreras = ({onSelect}) => {
     onSelect(carreraSeleccionada);
     setIsOpen(false);
   };
-  
-  
 
   return (
     <>
@@ -25,27 +48,14 @@ const OrdenarCarreras = ({onSelect}) => {
         value={selectedCarrera}
         required
       >
-        <option value="" >Seleccione una carrera</option>
-        {listCarrersOrden.map(({ nombre, id }) => (
-          <option value={nombre} key={id}>
-            {nombre}
+        <option value="">Seleccione una carrera</option>
+        {carreras.map(({ carrera, id }) => (
+          <option value={carrera} key={id}>
+            {carrera}
           </option>
         ))}
       </select>
-      {isOpen && (
-        <div>
-          {listCarrersOrden.map(({ nombre, id }) => (
-            <div
-              key={id}
-              onClick={() => {
-                setSelectedCarrera(nombre);
-                onSelect(nombre);
-                setIsOpen(false);
-              }}
-            ></div>
-          ))}
-        </div>
-      )}
+      
     </>
   );
 };
