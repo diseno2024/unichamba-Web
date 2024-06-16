226f8529792032c49974362cbc9659dca80b9e6d
Elvis Baños <bm20001@ues.edu.sv>
2024-06-16 09:59:23 -0600

    WhatsAppButton
    Arreglado el icono del componente.

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -5,8 +5,8 @@ import React from "react";
 const WhatsAppButton = ({phoneNumber}) => {
     const link = `https://wa.me/${phoneNumber}`
     return (
-        <a href={link} target="_blank" className="inline-flex items-center justify-center p-[10px_15px] bg-WhatsApp text-white border-none rounded-[5px] text-[16px] font-roboto font-semibold">
-            <box-icon type='logo' name='whatsapp' color='white' class='w-[24px] mr-[10px]'></box-icon>
+        <a href={link} target="_blank" className="inline-flex items-center justify-center p-[10px_15px] bg-WhatsApp text-white rounded-lg text-[16px] font-roboto font-semibold">
+            <img src="./public/whatsapp.svg" className="mr-2" alt="" />
             Contacta conmigo
         </a>
     )
