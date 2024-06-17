916bdeb4700985e9bb563f389659e9d3330651dd
eliasronquillo00 <elias.ronquillo00@gmail.com>
2024-06-17 12:33:32 -0600

    Editar perfil estudiante (incompleto)

---
+++ src/components/EditarPerfil.jsx
@@ -0,0 +1,85 @@
+import { useLocation } from "react-router-dom";
+import Swal from "sweetalert2";
+import { db } from "../data/firebase";
+import {doc,updateDoc} from "firebase/firestore";
+
+const EditarPerfil = ({ titulo, referencia, estudiante}) => {
+  const location = useLocation();
+  const {id} = estudiante;
+
+  const actualizarDato = async (referencia, id, dato) => {
+    if (referencia === "nombre") {
+        await updateDoc(doc(db, "estudiantes", id), {
+            nombre: dato,
+          });
+    } else if (referencia === "apellido") {
+        await updateDoc(doc(db, "estudiantes", id), {
+            apellido: dato,
+          });
+    } else if (referencia === "telefono") {
+      await updateDoc(doc(db, "estudiantes", id), {
+        telefono: dato,
+      });
+    } else if (referencia === "email") {
+      await updateDoc(doc(db, "estudiantes", id), {
+        email: dato,
+      });
+    } else if (referencia === "carrera") {
+      await updateDoc(doc(db, "estudiantes", id), {
+        carrera: dato,
+      });
+    } else if (referencia === "acercaDe") {
+      await updateDoc(doc(db, "estudiantes", id), {
+        acercaDe: dato,
+      });
+    }
+  }
+
+  const editarNombres = () => {
+    Swal.fire({
+      title: `Editar ${titulo}`,
+      html: `
+          <div>
+              <label style="display: block; padding-bottom:10px;">${titulo}</label>
+              <input id="nuevo" type="text" name="nuevo" placeholder="Escriba aqui" class="swal2-input" required>
+          </div>
+          `,
+      showCancelButton: true,
+      cancelButtonText: "Cancelar",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Agregar",
+      confirmButtonColor: "#04061A",
+      preConfirm: async () => {
+        const nuevoDato = document.getElementById("nuevo").value;
+
+        if (nuevoDato != "") {
+            actualizarDato(referencia, id, nuevoDato)
+          Swal.fire({
+            title: "Nombre editado con exito",
+            icon: "success",
+          });
+        } else {
+          Swal.fire({
+            title: "Error al modificar el nombre",
+            icon: "error",
+          });
+        }
+      },
+    });
+  };
+
+  return (
+    <>
+      {location.pathname === "/studentProfile" ? (
+        <span
+          className="material-symbols-outlined justify-end opacity-0 hover:opacity-100 top-0 right-0 transition-opacity duration-200"
+          onClick={editarNombres}
+        >
+          edit
+        </span>
+      ) : null}
+    </>
+  );
+};
+
+export default EditarPerfil;
--- src/pages/StudentProfile.jsx
+++ src/pages/StudentProfile.jsx
@@ -1,48 +1,57 @@
 import { NavLink, useLocation, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { addDoc, collection, doc, getDocs, updateDoc} from 'firebase/firestore';
-import { db, storage } from '../data/firebase';
+import {
+  addDoc,
+  collection,
+  doc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
+import { db, storage } from "../data/firebase";
 import { UserAuth } from "../context/AuthContext";
 import Swal from "sweetalert2";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import WhatsAppButton from "../components/WhatsAppButton";
+import EditarPerfil from "../components/EditarPerfil";
 
-
-// Este es la paginación de Elias  
+// Este es la paginación de Elias
 
 const StudentProfile = () => {
   const location = useLocation();
-  const {idPerfil} = useParams();
+  const { idPerfil } = useParams();
   const [estudiante, setEstudiante] = useState([]);
-  const [pdf, setPdf] = useState(null)
-  const {user} = UserAuth();
-  const initialStateValues = {pdfUrl: ""}
-  const [value, setValue] = useState(initialStateValues)
+  const [pdf, setPdf] = useState(null);
+  const { user } = UserAuth();
+  const initialStateValues = { pdfUrl: "" };
+  const [value, setValue] = useState(initialStateValues);
   let student = [];
-  
+
   const fetchData = async () => {
-    const studentsSnapshot = await getDocs(collection(db, 'estudiantes'));
-    const estudiantes = studentsSnapshot.docs.map(doc => ({...doc.data(), id: doc.id}));
-    const perfilSeleccionado = estudiantes.find(estudiante => estudiante.id === idPerfil)
+    const studentsSnapshot = await getDocs(collection(db, "estudiantes"));
+    const estudiantes = studentsSnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    const perfilSeleccionado = estudiantes.find(
+      (estudiante) => estudiante.id === idPerfil
+    );
     student = estudiantes;
 
     if (location.pathname === "/studentProfile") {
-      student.map(perfil => {
+      student.map((perfil) => {
         if (perfil.email === user.email) {
-          setEstudiante(perfil)
+          setEstudiante(perfil);
         }
-      }) 
-    }else{
-      setEstudiante(perfilSeleccionado)
+      });
+    } else {
+      setEstudiante(perfilSeleccionado);
     }
-    }
+  };
 
-    const {trabajos} = estudiante;
-
-
+  const { trabajos } = estudiante;
 
   const handlePDFChange = (e) => {
-    let archivo = document.getElementById('archivo');
+    let archivo = document.getElementById("archivo");
     let archivoRuta = archivo.value;
     let extPermitidas = /(.pdf)$/i;
 
@@ -50,14 +59,14 @@ const StudentProfile = () => {
       Swal.fire({
         title: "Error",
         icon: "error",
-        text: "Asegúrate de subir un PDF"
-      })
-      archivo.value = '';
-      return false
+        text: "Asegúrate de subir un PDF",
+      });
+      archivo.value = "";
+      return false;
     } else {
-      setPdf(e.target.files[0])
+      setPdf(e.target.files[0]);
     }
-  }
+  };
 
   const addOrEdit = async (link) => {
     try {
@@ -70,168 +79,188 @@ const StudentProfile = () => {
         return fileUrl;
       };
 
-      const url = await uploadPdf(pdf)
+      const url = await uploadPdf(pdf);
 
-      await updateDoc(doc(db, "estudiantes", docId), {pdfUrl: url});
-      let archivo = document.getElementById('archivo');
+      await updateDoc(doc(db, "estudiantes", docId), { pdfUrl: url });
+      let archivo = document.getElementById("archivo");
       archivo.value = "";
 
       Swal.fire({
         title: "PDF agregado",
         icon: "success",
-        text: "El PDF se agrego correctamente"
-      })
+        text: "El PDF se agrego correctamente",
+      });
     } catch (error) {
       console.log(error);
     }
-  }
-
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(value)
-    console.log(pdf)
-    addOrEdit(value)
-  }
+    console.log(value);
+    console.log(pdf);
+    addOrEdit(value);
+  };
 
   useEffect(() => {
-    fetchData()
+    fetchData();
 
     return () => {
       fetchData();
-    }
-  }, [])
-  
+    };
+  }, []);
 
   return (
     <>
-    {/* HEADER */}
-    <header>
-      <nav className="h-[90px] flex items-center justify-between px-10 bg-Dark-Blue shadow-md shadow-Gris-claro fixed top-0 w-full z-50">
+      {/* HEADER */}
+      <header>
+        <nav className="h-[90px] flex items-center justify-between px-10 bg-Dark-Blue shadow-md shadow-Gris-claro fixed top-0 w-full z-50">
           <NavLink to="/inicio">
             <img src="/LOGO.svg" alt="LOGO UNICHAMBA AZUL" />
           </NavLink>
-      </nav>
-    </header>
+        </nav>
+      </header>
 
+      {/* MAIN */}
+      <main className=" mb-10 mt-24">
+        {/* FOTO DE PERFIL Y PORTADA */}
+        <div className="realtive">
+          <div className=" mt-2 mx-5 bg-portada bg-cover h-[290px]">
+            <NavLink
+              to="/"
+              className="h-[50px] w-[80px] px-5 border-[1px]  border-transparent rounded-lg placeholder:text-white focus:outline-none mr-3 flex  text-white"
+            >
+              <button>
+                <span class="material-symbols-outlined">arrow_back</span>
+              </button>
+            </NavLink>
+          </div>
+          <div className=" w-[200px]  h-[200px] ml-9 rounded-full overflow-hidden flex items-center absolute top-60 left-5 border-4">
+            <img src={estudiante.imageUrl} alt="" className=" " />
+          </div>
 
-    {/* MAIN */}
-    <main className=" mb-10 mt-24">
-
-      {/* FOTO DE PERFIL Y PORTADA */}
-      <div className="realtive">
-
-      <div className=" mt-2 mx-5 bg-portada bg-cover h-[290px]">
-        <NavLink
-            to="/"
-            className="h-[50px] w-[80px] px-5 border-[1px]  border-transparent rounded-lg placeholder:text-white focus:outline-none mr-3 flex  text-white"
-          >
-            <button>
-              <span class="material-symbols-outlined">
-              arrow_back
-              </span>
-            </button>
-          </NavLink>
-      </div>
-
-      <div className=" w-[200px]  h-[200px] ml-9 rounded-full overflow-hidden flex items-center absolute top-60 left-5 border-4">
-        <img src={estudiante.imageUrl} alt="" className=" "/>
-      </div>
-
-      <div className="absolute right-6 pt-5">
-        {
-          trabajos && trabajos.map(trabajo => (
-            <span class="material-symbols-outlined text-Dark-Blue text-5xl">
-              {trabajo.icono}
-            </span>
-          ))
-        }
-
-        </div>
-      </div>
-
-      {/* INFORMACION */}
-      <div className="flex justify-between px-5 w-[97%] mx-auto relative mt-[85px]"> 
-
-        <div className="w-[20%] py-5">
-
-            <h2 className="ml-5 text-3xl font-normal">{estudiante.nombre}</h2>
-            <h1 className="ml-[26px] text-2xl font-normal">{estudiante.apellido}</h1>
-
-          <div className=" mt-5 ">
-          <span className=" ml-5 font-normal">Informacion personal</span>
-          <ul className=" mt-5 ml-4">
-            <li className="flex items-center">
-              <span class="material-symbols-outlined">call</span>
-              <span className=" ml-2 font-normal">Telefono fijo</span>
-            </li>
-            <p className=" ml-9 mb-3 font-light">{estudiante.telefono}</p>
-
-            <li className="flex items-center">
-              <span class="material-symbols-outlined">mail</span>
-              <span className=" ml-2 font-normal">Email</span>
-            </li>
-              <p className=" ml-9 mb-3 font-light">{estudiante.email}</p>
-
-            <li>
-              <span class="material-symbols-outlined">apartment</span>
-              <span className=" ml-2 font-normal">Educacion actual</span>
-            </li>
-              <p className=" ml-9 font-light">{estudiante.carrera}</p>
-          </ul>
-          <WhatsAppButton phoneNumber={estudiante.whatsapp}/>
+          <div className="absolute right-6 pt-5">
+            {trabajos &&
+              trabajos.map((trabajo) => (
+                <span class="material-symbols-outlined text-Dark-Blue text-5xl">
+                  {trabajo.icono}
+                </span>
+              ))}
           </div>
         </div>
 
+        {/* INFORMACION */}
+        <div className="flex justify-between px-5 w-[97%] mx-auto relative mt-[85px]">
+          <div className="w-[20%] py-5">
+            <h2 className="ml-5 text-3xl font-normal">
+              {estudiante.nombre}
+              <EditarPerfil titulo={"nombre"} referencia={"nombre"} estudiante={estudiante}/>
+            </h2>
+            <h1 className="ml-[26px] text-2xl font-normal">
+              {estudiante.apellido}
+              <EditarPerfil titulo={"apellido"} referencia={"apellido"} estudiante={estudiante}/>
+            </h1>
+            <div className=" mt-5 ">
+              <span className=" ml-5 font-normal">Informacion personal</span>
+              <ul className=" mt-5 ml-4">
+                <li className="flex items-center">
+                  <span class="material-symbols-outlined">call</span>
+                  <span className=" ml-2 font-normal">Telefono fijo
+                  <EditarPerfil titulo={"telefono"} referencia={"telefono"} estudiante={estudiante}/>
+                  </span>
+                </li>
+                <p className=" ml-9 mb-3 font-light">{estudiante.telefono}
+                
+                </p>
 
-        <div className="w-[80%]">
-          <h3 className=" ml-5 text-2xl font-normal">Acerca de</h3>
-          <div className=" w-[100%] ml-5 font-light text-lg">
-            <p>{estudiante.acercaDe}</p>
-          </div>
-          <div className="grid grid-cols-2 mt-10">
-            <div className=" ml-5">
-              <h3 className=" text-2xl font-normal">Experiencias en trabajos</h3>
-              <ul className=" font-light space-y-1 pt-2 text-lg">
-              {
-                  trabajos && trabajos.map(trabajo => (
-                  <li key={trabajo.id}>{trabajo.nombre}</li>
-                ))
-              }
+                <li className="flex items-center">
+                  <span class="material-symbols-outlined">mail</span>
+                  <span className=" ml-2 font-normal">Email
+                  <EditarPerfil titulo={"email"} referencia={"email"} estudiante={estudiante}/>
+                  </span>
+                </li>
+                <p className=" ml-9 mb-3 font-light">{estudiante.email}
+                
+                </p>
+
+                <li>
+                  <span class="material-symbols-outlined">apartment</span>
+                  <span className=" ml-2 font-normal">Educacion actual
+                  <EditarPerfil titulo={"carrera"} referencia={"carrera"} estudiante={estudiante}/>
+                  </span>
+                </li>
+                <p className=" ml-9 font-light">{estudiante.carrera}
+                
+                </p>
               </ul>
+              <WhatsAppButton phoneNumber={estudiante.whatsapp} />
             </div>
-            <div className=" ml-5">
-              <h3 className=" text-2xl font-normal">Curriculum</h3>
-              {location.pathname === "/studentProfile" ? (
+          </div>
+
+          <div className="w-[80%]">
+            <h3 className=" ml-5 text-2xl font-normal">Acerca de
+            <EditarPerfil titulo={"acerca de"} referencia={"acercaDe"} estudiante={estudiante}/>
+            </h3>
+            <div className=" w-[100%] ml-5 font-light text-lg">
+              <p>{estudiante.acercaDe}</p>
+            </div>
+            <div className="grid grid-cols-2 mt-10">
+              <div className=" ml-5">
+                <h3 className=" text-2xl font-normal">
+                  Experiencias en trabajos
+                </h3>
+                <ul className=" font-light space-y-1 pt-2 text-lg">
+                  {trabajos &&
+                    trabajos.map((trabajo) => (
+                      <li key={trabajo.id}>{trabajo.nombre}</li>
+                    ))}
+                </ul>
+              </div>
+              <div className=" ml-5">
+                <h3 className=" text-2xl font-normal">Curriculum</h3>
+                {location.pathname === "/studentProfile" ? (
+                  <div>
+                    <form onSubmit={handleSubmit}>
+                      <label className="font-light">
+                        Suba aqui su Curriculum
+                      </label>
+                      <input
+                        type="file"
+                        id="archivo"
+                        className="px-1"
+                        onChange={handlePDFChange}
+                      />
+                      <button className="bg-Rich-black text-white font-normal p-2 rounded-lg block mt-3">
+                        Enviar
+                      </button>
+                    </form>
                     <div>
-                      <form onSubmit={handleSubmit}>
-                        <label className="font-light">Suba aqui su Curriculum</label>
-                        <input type="file" id="archivo" className="px-1" onChange={handlePDFChange} />
-                        <button className="bg-Rich-black text-white font-normal p-2 rounded-lg block mt-3">
-                          Enviar
-                        </button>
-                      </form>
-                      <div>
-                        <a href={estudiante.pdfUrl} target="_blank" className="font-bold mt-2 block">
-                          Curriculum
-                        </a>
-                      </div>
-                    </div>
-                  ) : (
-                    <div>
-                      <a href={estudiante.pdfUrl} target="_blank" className="font-bold mt-2 block">
+                      <a
+                        href={estudiante.pdfUrl}
+                        target="_blank"
+                        className="font-bold mt-2 block"
+                      >
                         Curriculum
                       </a>
                     </div>
-                  )
-                }
+                  </div>
+                ) : (
+                  <div>
+                    <a
+                      href={estudiante.pdfUrl}
+                      target="_blank"
+                      className="font-bold mt-2 block"
+                    >
+                      Curriculum
+                    </a>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
-
-      </div>
-    </main>
+      </main>
     </>
-  )
-}
+  );
+};
 
 export default StudentProfile;
